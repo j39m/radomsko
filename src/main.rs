b99a099e0a69f8ac744022fc227ffeb5d36c467e
j39m <hikaph@gmail.com>
2021-02-06 12:09:02 -0800

    main: add basic argument handling
    This change outlines the three subcommands that radomsko will
    handle and adds basic argument parsing for the same.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-mod filesystem;
+pub mod filesystem;
+
+fn subcommand_not_implemented() {
+    eprintln!("This subcommand is not implemented.");
+    std::process::exit(1);
+}
 
 fn main() {
-    println!("Hello there!");
+    let matches = clap::App::new("radomsko")
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+        .about("interacts with your password store")
+        .version("0.1.0")
+        .author("j39m")
+        .subcommand(clap::App::new("show").about("decrypts passwords"))
+        .subcommand(clap::App::new("edit").about("edits passwords"))
+        .subcommand(clap::App::new("find").about("searches password store")).get_matches();
+
+    match matches.subcommand_name() {
+        Some("show") => return subcommand_not_implemented(),
+        Some("edit") => return subcommand_not_implemented(),
+        Some("find") => return subcommand_not_implemented(),
+        _ => panic!("BUG: unhandled subcommand"),
+    }
 }
